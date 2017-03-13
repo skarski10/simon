4bfc9b2373019e26a6b4f045af80f52fdc703c75
brandon cooper and kory skarbek <brandon+kory@>
2017-03-13 11:32:15 -0700

    add div styling to display. add simon function for random color

---
+++ js/simon.js
@@ -0,0 +1,16 @@
+var colorSelect = [];
+function simon(){
+  var result = Math.floor((Math.random() * 4) + 1);
+  if (result = 1) {
+    colorSelect.push("red");
+  }
+  if (result = 2) {
+    colorSelect.push("blue");
+  }
+  if (result = 3) {
+    colorSelect.push("green");
+  }
+  if (result = 4) {
+    colorSelect.push("yellow");
+  }
+}
